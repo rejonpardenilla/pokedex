456b4f05de97da489891620a124a0283ea5da817
Daniel Rejon <rejonpardenilla@gmail.com>
2020-05-12 10:18:01 -0500

    Fix concern

--- app/models/concerns/can_learn_moves_with_level_up.rb
+++ app/models/concerns/can_learn_moves_with_level_up.rb
@@ -1,4 +1,8 @@
 module CanLearnMovesWithLevelUp
-  has_many :move_by_level_ups
-  has_many :moves_learnt_by_level_up, through: :move_by_level_ups, source: :move
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :move_by_level_ups
+    has_many :moves_learnt_by_level_up, through: :move_by_level_ups, source: :move
+  end
 end
